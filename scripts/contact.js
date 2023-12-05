43e4377d8fd478a848655fb5666abb973c3672c7
Mathew Ross <matjross@gmail.com>
2023-12-04 17:14:43 -0700

    adding semicolons to contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,10 +2,10 @@
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
-let submitButton = document.getElementById('submit-button')
-let mainContent = document.getElementById('contact-page')
+let submitButton = document.getElementById('submit-button');
+let mainContent = document.getElementById('contact-page');
 
 submitButton.addEventListener('click', function() {
   mainContent.innerHTML = "Thank you for your message!";
   mainContent.style.fontSize = '24px';
-})
+});
